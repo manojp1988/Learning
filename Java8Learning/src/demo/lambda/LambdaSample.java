7a4bf5cefe6d932968c88a104546dae7b642def6
manoj <manoj@192.168.2.3>
2015-02-10 23:22:58 -0500

    formatted

--- Java8Learning/src/demo/lambda/LambdaSample.java
+++ Java8Learning/src/demo/lambda/LambdaSample.java
@@ -5,42 +5,28 @@ import java.util.function.Predicate;
 public class LambdaSample {
 
   public static void main(String[] args) {
-
     System.out.println(checkIfGreaterInOlderWay(new ICompare() {
-
       @Override
       public boolean isGtThan(int x) {
         return x > 10;
       }
     }, 2));
-
     System.out.println(checkIfGreaterInLamdaWay(x -> x > 10, 2));
-
     System.out.println(checkIfGreaterInLamdaWay(LambdaSample::isGtThan, 2));
-
-
-
   }
 
   public static boolean isGtThan(int xxValue) {
     return xxValue > 10;
   }
 
-
-
   public static boolean checkIfGreaterInLamdaWay(Predicate<Integer> func, int x) {
     return func.test(x);
   }
 
-
-
   public static boolean checkIfGreaterInOlderWay(ICompare s, int x) {
     return s.isGtThan(x);
   }
-
-
 }
-
 
 
 interface ICompare {
