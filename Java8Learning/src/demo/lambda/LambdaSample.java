2721f494416cf49fd058a564d1b16e184b9f3271
manoj <manoj@192.168.2.3>
2015-02-10 23:21:58 -0500

    Older and newer way

--- Java8Learning/src/demo/lambda/LambdaSample.java
+++ Java8Learning/src/demo/lambda/LambdaSample.java
@@ -1,5 +1,5 @@
 package demo.lambda;
-import java.util.function.Function;
+
 import java.util.function.Predicate;
 
 public class LambdaSample {
@@ -18,13 +18,7 @@ public class LambdaSample {
 
     System.out.println(checkIfGreaterInLamdaWay(LambdaSample::isGtThan, 2));
 
-    System.out.println(getSquareOfX(x -> x * x, 2));
 
-    System.out.println(getSquareOfX(LambdaSample::sqOfX, 2));
-
-    System.out.println(LambdaSample.sqOfX(2));
-
-    System.out.println(submit(x -> "Jo"));
 
   }
 
@@ -32,40 +26,22 @@ public class LambdaSample {
     return xxValue > 10;
   }
 
-  public static int sqOfX(int x) {
-    return x * x;
-  }
+
 
   public static boolean checkIfGreaterInLamdaWay(Predicate<Integer> func, int x) {
     return func.test(x);
   }
 
-  public static int getSquareOfX(Function<Integer, Integer> func, int x) {
-    return func.apply(x);
-  }
+
 
   public static boolean checkIfGreaterInOlderWay(ICompare s, int x) {
     return s.isGtThan(x);
   }
 
-  public static void submit(A a) {
-    a.display();
-  }
-
-  public static String submit(B b) {
-
-    return "H";
-  }
 
 }
 
-interface A {
-  void display();
-}
 
-interface B {
-  String sayHello(String a);
-}
 
 interface ICompare {
   boolean isGtThan(int x);
