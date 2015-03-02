70a25f2ef4953e88c6063579a74b6ae7a8cc1103
manojp1988 <manojp1988@gmail.com>
2015-03-01 23:52:09 -0500

    SpringMVC

---
+++ Spring Tutorials/SpringMVC/src/main/java/controllers/FriendsController.java
@@ -0,0 +1,46 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FriendsController {
+
+	@RequestMapping(method=RequestMethod.GET, value="/friends")
+	public String listofFriends(Model model){
+		List<String> friendsList = new ArrayList<String>();
+		for(int i=0; i<5; i++){
+			friendsList.add(i+" :");
+		}
+		model.addAttribute("friends", friendsList);
+		return "friends";
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/Age")
+	public String getAge(Model model, @RequestParam("age") int age){
+		List<String> friendsList = new ArrayList<String>();
+		for(int i=0; i<5; i++){
+			friendsList.add(i+" :"+ age);
+		}
+		model.addAttribute("friends", friendsList);
+		return "friends";
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/friends/{num}")
+	public String oneFriends(Model model, @PathVariable int num){
+		List<String> friendsList = new ArrayList<String>();
+		for(int i=0; i<num; i++){
+			friendsList.add(i+" :");
+		}
+		model.addAttribute("friends", friendsList);
+		return "friends";
+	}
+	
+}
