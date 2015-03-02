f8782c0c3cccc082423831a1c41efa53ad1043c6
manojp1988 <manojp1988@gmail.com>
2015-03-02 00:06:55 -0500

    Form submission

---
+++ Spring Tutorials/SpringMVC/src/main/java/beans/Person.java
@@ -0,0 +1,34 @@
+package beans;
+
+public class Person {
+
+	private String firstName1;
+	private String lastName;
+	private String userName;
+	private String  password;
+	
+	public String getFirstName() {
+		return firstName1;
+	}
+	public void setFirstName(String firstName1) {
+		this.firstName1 = firstName1;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
--- Spring Tutorials/SpringMVC/src/main/java/controllers/FriendsController.java
+++ Spring Tutorials/SpringMVC/src/main/java/controllers/FriendsController.java
@@ -10,37 +10,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import beans.Person;
+
 @Controller
 public class FriendsController {
 
-	@RequestMapping(method=RequestMethod.GET, value="/friends")
-	public String listofFriends(Model model){
+	@RequestMapping(method = RequestMethod.GET, value = "/friends")
+	public String listofFriends(Model model) {
 		List<String> friendsList = new ArrayList<String>();
-		for(int i=0; i<5; i++){
-			friendsList.add(i+" :");
+		for (int i = 0; i < 5; i++) {
+			friendsList.add(i + " :");
 		}
 		model.addAttribute("friends", friendsList);
 		return "friends";
 	}
-	
-	@RequestMapping(method=RequestMethod.GET, value="/Age")
-	public String getAge(Model model, @RequestParam("age") int age){
+
+	@RequestMapping(method = RequestMethod.GET, value = "/Age")
+	public String getAge(Model model, @RequestParam("age") int age) {
 		List<String> friendsList = new ArrayList<String>();
-		for(int i=0; i<5; i++){
-			friendsList.add(i+" :"+ age);
+		for (int i = 0; i < 5; i++) {
+			friendsList.add(i + " :" + age);
 		}
 		model.addAttribute("friends", friendsList);
 		return "friends";
 	}
-	
-	@RequestMapping(method=RequestMethod.GET, value="/friends/{num}")
-	public String oneFriends(Model model, @PathVariable int num){
+
+	@RequestMapping(method = RequestMethod.GET, value = "/friends/{num}")
+	public String oneFriends(Model model, @PathVariable int num) {
 		List<String> friendsList = new ArrayList<String>();
-		for(int i=0; i<num; i++){
-			friendsList.add(i+" :");
+		for (int i = 0; i < num; i++) {
+			friendsList.add(i + " :");
 		}
 		model.addAttribute("friends", friendsList);
 		return "friends";
 	}
-	
+
+	@RequestMapping(method = RequestMethod.GET, value = "/register")
+	public String getAddUserForm() {
+		return "register";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/register")
+	public String addUserForm(Person person) {
+		System.out.println("Person=" +person.getFirstName());
+		return "redirect:friends/"+1;
+	}
+
 }
