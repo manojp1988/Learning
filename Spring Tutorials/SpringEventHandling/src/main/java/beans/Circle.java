cebba8720cc16cdf6b64d0a878ac80374fd06dd5
manojp1988 <manojp1988@gmail.com>
2015-03-04 16:57:42 -0500

    @Autowired & @Qualifier explained..

--- Spring Tutorials/SpringEventHandling/src/main/java/beans/Circle.java
+++ Spring Tutorials/SpringEventHandling/src/main/java/beans/Circle.java
@@ -1,5 +1,7 @@
 package beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.stereotype.Component;
@@ -10,9 +12,16 @@ public class Circle implements ApplicationEventPublisherAware{
 	
 	ApplicationEventPublisher publisher;
 	
+	@Autowired
+	@Qualifier(value="pointA")
+	Point center;
+	
 	public void draw(){
 		System.out.println("Drawing.. ");
 		publisher.publishEvent(new MyAppEvent(this));
+		
+		System.out.println("X= "+center.getX());
+		System.out.println("Y= "+center.getY());
 	}
 
 	public void setApplicationEventPublisher(ApplicationEventPublisher arg0) {
--- Spring Tutorials/SpringEventHandling/src/main/java/config/SpringConfig.java
+++ Spring Tutorials/SpringEventHandling/src/main/java/config/SpringConfig.java
@@ -1,11 +1,25 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import beans.Circle;
+import beans.Point;
 
 
 @Configuration
 @ComponentScan(basePackages={"beans"})
 public class SpringConfig {
+	
+	@Bean
+	public Point pointA(){
+		return new Point(1,3);
+	}
+
+	@Bean
+	public Point pointB(){
+		return new Point(4,6);
+	}
 
 }
