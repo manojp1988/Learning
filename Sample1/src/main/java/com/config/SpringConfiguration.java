14cc1e67ce8e683440d01817a8c016ed1f092ab4
manojperiathambi <manojp1988@gmail.com>
2015-07-26 13:48:36 -0400

    Updated Question

--- Sample1/src/main/java/com/config/SpringConfiguration.java
+++ Sample1/src/main/java/com/config/SpringConfiguration.java
@@ -2,65 +2,42 @@ package com.config;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.annotation.Gateway;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.channel.interceptor.WireTap;
 import org.springframework.integration.config.EnableIntegration;
-import org.springframework.integration.config.GlobalChannelInterceptor;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.dsl.channel.MessageChannels;
-import org.springframework.integration.dsl.core.Pollers;
 import org.springframework.integration.dsl.support.GenericHandler;
-import org.springframework.integration.scheduling.PollerMetadata;
-import org.springframework.messaging.MessageChannel;
 
 
 /**
  * Created by manojperiathambi on 7/15/15.
  */
 @Configuration
-@ComponentScan(basePackages = "com.model")
 @EnableIntegration
-@IntegrationComponentScan
-@ImportResource(value="/applicationContext.xml")
+@IntegrationComponentScan(basePackages="com.config")
 public class SpringConfiguration {
 
-  @Bean
-  public MessageChannel traceChannel() {
-    return MessageChannels.direct("TRACE_EL_CHANL")
-                          .interceptor(new WireTap(loggerChannel()))
-                          .get();
-  }
-
-  @Bean
-  public MessageChannel loggerChannel() {
-    return MessageChannels.queue()
-                          .get();
-  }
-
+  @Autowired B b;
+  
+  @Autowired WireTap wireTap;
+  
   @Bean
   public IntegrationFlow sayHelloFlow() {
     return IntegrationFlows.from("say_EL_Hello")
                            .handle(new GenericHandler<String>() {
-
                              public String handle(String payload, Map headers) {
-                               // TODO Auto-generated method stub
+                               System.out.println(b.getName());
+                               System.out.println(wireTap.hashCode());
                                return "Hello " + payload;
                              }
                            })
                            .get();
-  }
-
-  @Bean(name = PollerMetadata.DEFAULT_POLLER)
-  public PollerMetadata poller() { // 12
-    return Pollers.fixedDelay(1000)
-                  .get();
   }
 
   @MessagingGateway
@@ -69,23 +46,6 @@ public class SpringConfiguration {
     public String sayHello(String msg);
   }
 
-  @Bean
-  @GlobalChannelInterceptor(patterns = "*_EL*", order=3)
-  public WireTap wireTap() {
-      return new WireTap(loggerChannel());
-  }
-  
-  
-  @Bean
-  public IntegrationFlow loggerIntFlow() {
-    return IntegrationFlows.from(loggerChannel())
-                           .handle(new GenericHandler<String>() {
-                            public Object handle(String payload, Map<String, Object> headers) {
-                              System.out.println("DSL Intercepted" + payload);
-                              return null;
-                            }})
-                           .channel("nullChannel")
-                           .get();
-  }
+
 
 }
--- Sample1/src/test/java/com/sample/SampleTest.java
+++ Sample1/src/test/java/com/sample/SampleTest.java
@@ -10,27 +10,25 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.support.DelegatingSmartContextLoader;
 
+import com.config.DSLInterceptor;
 import com.config.SpringConfiguration;
 import com.config.SpringConfiguration.helloService;
 import com.model.XmlService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextHierarchy({
- //@ContextConfiguration(locations = {"/applicationContext.xml"}),
- @ContextConfiguration(classes = SpringConfiguration.class),
+  @ContextConfiguration(classes = DSLInterceptor.class),
+  @ContextConfiguration(classes = SpringConfiguration.class),
  })
 public class SampleTest {
 
   @Autowired
   helloService hello;
 
-  @Autowired
-  XmlService xmlSer;
+
 
   @Test
   public void test() {
-    // System.out.println(hello.sayHello("Manoj"));
-
-    xmlSer.sayHello("Jeeva");
+    System.out.println(hello.sayHello("Manoj")); 
   }
 }
